bc3ad58360de51a0c0f9126ccb5d206390761aba
Stefano Garzarella <s.garzarella@evidence.eu.com>
2015-11-13 11:04:19 +0100

    steshell: fix code style

--- steshell.py
+++ steshell.py
@@ -13,35 +13,6 @@ import subprocess
 #   ss.ibash('grep -rin asd /')
 
 
-# cmd_a: array of commands
-# return stdout and stderr as list of lines
-def cmda(cmd_a, logger = None):
-    if logger != None:
-        logger.getChild('steshell.icmda').info(cmd_a)
-
-    process = subprocess.Popen(cmd_a, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    ret = process.communicate()
-    shout = ret[0].decode()
-    sherr = ret[1].decode()
-
-    if logger != None:
-        logger.getChild('steshell.cmda').info("out: " + shout)
-        logger.getChild('steshell.cmda').info("out: " + sherr)
-
-    return shout.splitlines(), sherr.splitlines()
-
-# cmd_a: array of commands
-# interactive cmd (print output -stdout and stderr- during execution)
-def icmda(cmd_a, logger = None):
-    if logger != None:
-        logger.getChild('steshell.cmda').info(cmd_a)
-
-    process = subprocess.Popen(cmd_a, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    for line in process.stdout:
-        print(line.decode(), end='')
-
-    return
-
 # cmd: sh command string
 def sh(cmd, logger = None):
     sh_cmd = []
@@ -73,3 +44,37 @@ def ibash(cmd, logger = None):
     sh_cmd.append('-c')
     sh_cmd.append(cmd)
     return icmda(sh_cmd, logger)
+
+
+
+######################### utilities #########################
+
+# cmd_a: array of commands
+# return stdout and stderr as list of lines
+def cmda(cmd_a, logger = None):
+    if logger != None:
+        logger.getChild('steshell.icmda').info(cmd_a)
+
+    process = subprocess.Popen(cmd_a, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    ret = process.communicate()
+    shout = ret[0].decode()
+    sherr = ret[1].decode()
+
+    if logger != None:
+        logger.getChild('steshell.cmda').info("out: " + shout)
+        logger.getChild('steshell.cmda').info("out: " + sherr)
+
+    return shout.splitlines(), sherr.splitlines()
+
+# cmd_a: array of commands
+# interactive cmd (print output -stdout and stderr- during execution)
+def icmda(cmd_a, logger = None):
+    if logger != None:
+        logger.getChild('steshell.cmda').info(cmd_a)
+
+    process = subprocess.Popen(cmd_a, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    for line in process.stdout:
+        print(line.decode(), end='')
+
+    return
+
