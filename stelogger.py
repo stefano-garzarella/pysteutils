0fbb7ef52fce5b3cee6e0156567d8afdddb71363
Stefano Garzarella <s.garzarella@evidence.eu.com>
2015-12-03 14:02:54 +0100

    Add SteLogger class

--- stelogger.py
+++ stelogger.py
@@ -1,24 +1,84 @@
 import logging
+import inspect
+import datetime
 
-# create simple application logger (on file)
+class SteLogger:
+    """
+    Simple application logger (on file)
 
-# return logger that use appname.log file
-#   appname: application name
-def setup_logger(appname):
-    logger = logging.getLogger(appname)
-    logger.setLevel(logging.DEBUG)
-    # create file handler which logs even debug messages
-    fh = logging.FileHandler(appname + '.log')
-    fh.setLevel(logging.DEBUG)
-    # create console handler with a higher log level
-    ch = logging.StreamHandler()
-    ch.setLevel(logging.ERROR)
-    # create formatter and add it to the handlers
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    fh.setFormatter(formatter)
-    ch.setFormatter(formatter)
-    # add the handlers to the logger
-    logger.addHandler(fh)
-    logger.addHandler(ch)
+    Example:
 
-    return logger
+    from pysteutils.stelogger import SteLogger
+
+    # log file = AppName.log
+    logger = SteLogger('AppName')
+    logger.debug('test dbg msg')
+
+    """
+    logger = None
+
+    def __init__(self, appname = 'stelogger'):
+        """
+        Constructor
+
+        :type   appname: String
+        :param  appname: application name (log stored in <appname>.log)
+        """
+        self.logger = logging.getLogger(appname)
+        self.logger.setLevel(logging.DEBUG)
+        # create file handler which logs even debug messages
+        fh = logging.FileHandler(appname + '.log')
+        fh.setLevel(logging.DEBUG)
+        # create console handler with a higher log level
+        ch = logging.StreamHandler()
+        ch.setLevel(logging.ERROR)
+        # create formatter and add it to the handlers
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        fh.setFormatter(formatter)
+        ch.setFormatter(formatter)
+        # add the handlers to the logger
+        self.logger.addHandler(fh)
+        self.logger.addHandler(ch)
+
+    def error(self, msg = ''):
+        """
+        Log error message with filename, line, funcname
+
+        :type   msg: String
+        :param  msg: message to print
+        """
+        # get the previous frame in the stack
+        func = inspect.currentframe().f_back.f_code
+        self.logger.error("%s:%i %s(): %s" % (
+                func.co_filename,
+                func.co_firstlineno,
+                func.co_name,
+                msg,
+        ))
+
+    def debug(self, msg = ''):
+        """
+        Log debug message with filename, line, funcname
+
+        :type   msg: String
+        :param  msg: message to print
+        """
+        # get the previous frame in the stack
+        func = inspect.currentframe().f_back.f_code
+        self.logger.debug("%s:%i %s(): %s" % (
+                func.co_filename,
+                func.co_firstlineno,
+                func.co_name,
+                msg,
+        ))
+
+    def print(self, msg = ''):
+        """
+        Print message with time
+
+        :type   msg: String
+        :param  msg: message to print
+        """
+        now = str(datetime.datetime.now())
+        print(now + ' - ' + msg)
+        self.logger.info((msg))
